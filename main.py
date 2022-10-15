868c38a70d60378c7b3bf276c2a54eda72ed40a2
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-15 09:37:23 +0300

    lab 2 done

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 
 #
+z=int(input("Введіть висоту прямокутника: "))
+y=int(input("Введіть ширину прямокутника: "))
+for i in range(z):
+    for j in range(y):
+        print(f"*", end="")
+    print()
