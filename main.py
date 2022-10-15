67f60d7338c2d8c2929a449260d5223aab9f31c6
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-15 11:19:48 +0300

    lab 4 done

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 
 #
+try:
+    dovzh = int(input("Введіть довжину прямокутника: "))
+    shir = int(input("Введіть ширину прямокутника: "))
+    for i in range(dovzh):
+        for j in range(shir):
+            if i == 0 or i == dovzh-1:
+                print("* ", end=" ")
+            else:
+                if j == 0 or j == shir-1:
+                    print("* ", end=" ")
+                else:
+                    print("  ", end=" ")
+        print()
+
+
+
+except Exception as ex:
+    print(ex)
