05adb71ef2d2a19699770ec3dac7b40d6ec80d4f
yaros <yaroslavdisiak@gmail.com>
2022-10-15 15:16:09 +0300

    HM 2 first update

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 
 #
+try:
+    dovzh = int(input("Enter the number of lines in length: "))
+    shir = int(input("Enter the number of rows in width: "))
+    print("========================================Multiplication table=========================================")
+
+
+    for i in range(dovzh+1):
+        for j in range(shir+1):
+
+            if i > 0 and j > 0:
+                print(f"{i}*{j}={int(i * j)}  ", end="")
+            else:
+                print(" ", end=" ")
+
+
+        print()
+
+    print("====================================================================================================")
+
+
+except Exception as ex:
+    print(ex)
