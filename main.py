d32f7aee735b40c4ec6cc32b3bc47268e199529e
yaros <yaroslavdisiak@gmail.com>
2022-10-15 15:47:29 +0300

    HM 3 first update

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 
 #
+try:
+    start = int(input("Enter the start of the multiplication range: "))
+    end = int(input("Enter the end of the multiplication range: "))
+    print("========================================Multiplication table=========================================")
+
+
+    for i in range(start,end+1):
+        for j in range(10+1):
+
+            if i >0 and j>0 :
+                print(f"{i}*{j}={int(i * j)}  ", end="")
+            else:
+                print(" ", end=" ")
+
+
+        print()
+
+    print("====================================================================================================")
+
+
+except Exception as ex:
+    print(ex)
