cf2a9091f3646dc835fffd92243284e2efa6d076
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-15 11:16:44 +0300

    lab 3 done

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 
 #
+try:
+    user = int(input("Введіть розмір сторони квадрата: "))
+    for i in range(user):
+        for j in range(user):
+            if i == 0 or i == user-1:
+                print("* ", end=" ")
+            else:
+                if j == 0 or j == user-1:
+                    print("* ", end=" ")
+                else:
+                    print("  ", end=" ")
+        print()
+
+
+
+except Exception as ex:
+    print(ex)
+
+
+
+
+
+
