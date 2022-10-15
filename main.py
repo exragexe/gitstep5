14eb6c16ea08780733e9dbc03edd3c9c51be00e2
yaros <yaroslavdisiak@gmail.com>
2022-10-15 13:10:29 +0300

    lab 2 done second update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 #
-z=int(input("Введіть висоту прямокутника: "))
-y=int(input("Введіть ширину прямокутника: "))
-for i in range(z):
-    for j in range(y):
-        print(f"*", end="")
-    print()
+try:
+    z = int(input("Введіть висоту прямокутника: "))
+    y = int(input("Введіть ширину прямокутника: "))
+    for i in range(z):
+        for j in range(y):
+            print(f"*", end="")
+        print()
+except Exception as ex:
+    print(ex)
